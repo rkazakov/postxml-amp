c00fe3189ea8cf9ea0b79962b8a50d855e1260d2
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-03-01 21:24:28 +1100

    Code refactoring

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 var fs = require('fs');
 var sizeOf = require('image-size');
 var request = require('sync-request');
+var cleanCss = require('clean-css');
 
-module.exports = function (options) {
+module.exports = function(options) {
   var tags = {
     amp: ['img', 'video']
   };
 
   var round;
   var options = options || {};
+
   options.cwd = options.cwd || '';
   options.round = options.round || true;
+
   if (options.round) {
-    round = function (numb) {
-        return Math.round(numb / 5) * 5
-    }
-  } else {
-    round = function (numb) {
-        return numb;
-    }
+    round = function(numb) { return Math.round(numb / 5) * 5; }
+  }
+  else {
+    round = function(numb) { return numb; }
   }
 
-  return function ($) {
+  return function($) {
     /* html ⚡ */
-    $('html').each(function () {
+    $('html').each(function() {
       $(this).attr('amp', '');
     });
 
     /* amp-img, amp-video */
-    $(tags.amp.join(',')).each(function () {
+    $(tags.amp.join(',')).each(function() {
         this.name = 'amp-' + this.name;
     });
 
     /* local amp-img */
-    $('amp-img[src]:not([src*="//"]):not(width):not(height)').each(function () {
+    $('amp-img[src]:not([src*="//"]):not(width):not(height)').each(function() {
         var image = options.cwd + '/' + $(this).attr('src');
         if (fs.existsSync(image)) {
           var size = sizeOf(image);
@@ -45,7 +45,7 @@ module.exports = function (options) {
     });
 
     /* remote amp-img */
-    $('amp-img[src*="//"]:not(width):not(height)').each(function () {
+    $('amp-img[src*="//"]:not(width):not(height)').each(function() {
       var imageUrl = this.attribs.src;
       var response = request('GET', imageUrl);
       if (response.statusCode === 200) {
@@ -58,12 +58,13 @@ module.exports = function (options) {
     });
 
     /* inline styles */
-    $('link[rel=stylesheet]').each(function () {
+    $('link[rel=stylesheet]').each(function() {
       var src = $(this).attr('href');
       var path = src;
       var file = '';
       var setFile = function (data) {
-            return '<style amp-custom>' + data + '</style>';
+        var minified = new cleanCss().minify(data).styles;
+        return '<style amp-custom>' + minified + '</style>';
       };
 
       if (src.indexOf('//') === -1) {
