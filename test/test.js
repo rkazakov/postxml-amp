49ed56fe04565f66df1306ec14bbe15b02558ea7
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-03-01 21:24:04 +1100

    More tests

--- test/test.js
+++ test/test.js
@@ -1,43 +1,71 @@
-var plugin = require('../');
-var postxml = require('postxml');
+var path = require('path');
 var expect = require('chai').expect;
 
-var test = function (input, output, opts) {
+var postxml = require('postxml');
+var plugin = require('../');
+
+var test = function(input, output, opts) {
 	var processed = postxml([plugin(opts)]).process(input);
 	expect(processed).to.eql(output);
 };
 
-describe('postxml-amp', function () {
-	it('should add amp attribute on html tag', function () {
-		test(
-			'<html></html>',
-			'<html amp=""></html>',
-			{}
-		);
+describe('postxml-amp', function() {
+
+	describe('html amp tag', function() {
+
+		it('should add amp attribute on html tag', function () {
+			test(
+				'<html></html>',
+				'<html amp=""></html>',
+				{}
+			);
+		});
+
 	});
 
-	it('should replace img tag with amp-img', function () {
-		test(
-			'<img src="test.jpg" width="600" height="400">',
-			'<amp-img src="test.jpg" width="600" height="400"></amp-img>',
-			{}
-		);
+	describe('inline styles', function() {
+
+		it('should inject inline css into style tag', function () {
+			test(
+				'<link rel="stylesheet" href="style.css">',
+				'<style amp-custom="">body{background-color:#FFF}</style>',
+				{cwd: __dirname.split(path.sep).pop()}
+			);
+		});
+
 	});
 
-	xit('should add width and height attributes on amp-img tag', function () {
-		test(
-			'<img src="test.png">',
-			'<amp-img src="test.png"></amp-img>',
-			{}
-		);
+	describe('amp-img tag', function() {
+
+		it('should replace img tag with amp-img', function() {
+			test(
+				'<img src="image.png" width="600" height="400">',
+				'<amp-img src="image.png" width="600" height="400"></amp-img>',
+				{}
+			);
+
+		});
+
+		it('should add width and height attributes on amp-img tag', function() {
+			test(
+				'<img src="image.png">',
+				'<amp-img src="image.png" width="600" height="400"></amp-img>',
+				{cwd: __dirname.split(path.sep).pop()}
+			);
+		});
+
 	});
 
-	it('should replace video tag with amp-video', function () {
-		test(
-			'<video src="test.mpg">',
-			'<amp-video src="test.mpg"></amp-video>',
-			{}
-		);
+	describe('amp-video tag', function() {
+
+		it('should replace video tag with amp-video', function() {
+			test(
+				'<video src="test.mpg">',
+				'<amp-video src="test.mpg"></amp-video>',
+				{}
+			);
+		});
+
 	});
 
 });
