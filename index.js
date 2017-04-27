597b21884242eb5e7288ccfaab959940c6161671
Ruslan Kazakov <Ruslan.Kazakov@myob.com>
2017-04-27 15:41:49 +1000

    AMP generation improvements: charset to first position, lib to second position, remove invalid img

--- index.js
+++ index.js
@@ -29,29 +29,31 @@ module.exports = function(options) {
 
     /* head */
 
-    /* adding meta */
-    if ($('head meta[charset="utf-8"]').length === 0) {
-      $('head').append('<meta charset="utf-8">');
-    }
+		/* main amp library */
+		$('head script[src="https://cdn.ampproject.org/v0.js"]').remove();
+	  $('head').prepend('<script async src="https://cdn.ampproject.org/v0.js"></script>');
 
-    /* meta viewport */
-    if ($('head meta[content="width=device-width,minimum-scale=1,initial-scale=1"]').length === 0) {
-      $('head').append('<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">');
-    }
+		/* meta charset */
+	  $('head meta[charset="utf-8"]').remove();
+	  $('head meta[charset="UTF-8"]').remove();
+	  $('head').prepend('<meta charset="utf-8">');
 
-    /* style amp-boilerplate */
-    if ($('head style[amp-boilerplate]').length === 0) {
-      $('head').append('<style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate="">body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>');
-    }
+		/* meta viewport */
+		if ($('head meta[content="width=device-width,minimum-scale=1,initial-scale=1"]').length === 0) {
+	    $('head').append('<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">');
+	  }
 
-    /* adding main amp library */
-    if ($('head script[src="https://cdn.ampproject.org/v0.js"]').length === 0) {
-      $('head').append('<script async src="https://cdn.ampproject.org/v0.js"></script>');
-    }
+		/* style amp-boilerplate */
+	  if ($('head style[amp-boilerplate]').length === 0) {
+	    $('head').append('<style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate="">body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>');
+	  }
 
     /* img dimensions */
     $('img:not(width):not(height)').each(function() {
       var src = $(this).attr('src');
+			if (!src) {
+				return $(this).remove();
+			}
       if (src.indexOf('//') === -1) {
         var image = options.cwd + '/' + $(this).attr('src');
         if (fs.existsSync(image)) {
